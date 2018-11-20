ac7d2959cf5786187b3c73ccbde18c671687226f
whitestne0746 <shiraishi.re1554@gmail.com>
2018-11-20 15:23:07 +0900

    a

--- src/MainBuilding.js
+++ src/MainBuilding.js
@@ -14,8 +14,9 @@ export default class MainBuilding extends THREE.Object3D {
     const building = new THREE.Object3D()
     const tower = new THREE.Mesh(
       new THREE.CylinderGeometry(20, 40, 100, 4, 1, false),
-      new THREE.MeshPhongMaterial({ color: '#00ff00' })
+      new THREE.MeshPhongMaterial({ color: '#ffffff' })
     )
+    tower.position.y = 50
     building.add(tower)
     return building
   }
